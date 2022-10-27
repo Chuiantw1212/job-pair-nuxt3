093aa68c2d7d6a602d1afd6e421ae47766bc3ad9
Ian Chu <chuiantw1212@gmail.com>
2022-10-27 09:52:18 +0800

    add bootstrap style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     app: {
@@ -9,19 +7,22 @@ export default defineNuxtConfig({
             //     { charset: 'utf-8', },
             //     { name: 'description', content: "Job Pair is a platform for matching people and jobs. Through the patent of fit rate, job matching is no longer like finding a needle in a haystack." }
             // ],
-            // link: [
-            //     // Google Fonts
-            //     { rel: "preconnect", href: "https://fonts.googleapis.com" },
-            //     { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true },
-            //     { rel: "preload", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap", as: "style", onload: "this.onload=null;this.rel='stylesheet'", media: "screen", crossorigin: "anonymous" },
-            //     { rel: "preload", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap", as: "style", onload: "this.onload=null;this.rel='stylesheet'", media: "screen", crossorigin: "anonymous" },
-            //     // Firebase
-            //     { rel: "preload", href: "https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css", as: "style", onload: "this.onload=null;this.rel='stylesheet'", type: "text/css" },
-            //     // Flatpickr
-            //     { rel: "preload", href: "https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css", as: "style", onload: "this.onload=null;this.rel='stylesheet'", type: "text/css", media: "screen" },
-            //     // AOS
-            //     { rel: "preload", href: "https://unpkg.com/aos@next/dist/aos.css", as: "style", onload: "this.onload=null;this.rel='stylesheet'", type: "text/css", media: "screen" }
-            // ],
+            link: [
+                // Google Fonts
+                { rel: "preconnect", href: "https://fonts.googleapis.com" },
+                { rel: "preconnect", href: "https://fonts.gstatic.com", },
+                { rel: "preload", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap", as: "style", onload: "this.onload=null;this.rel='stylesheet'", media: "screen", crossorigin: "anonymous" },
+                { rel: "preload", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap", as: "style", onload: "this.onload=null;this.rel='stylesheet'", media: "screen", crossorigin: "anonymous" },
+                // Firebase
+                { rel: "preload", href: "https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css", as: "style", onload: "this.onload=null;this.rel='stylesheet'", type: "text/css" },
+                // Flatpickr
+                { rel: "preload", href: "https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css", as: "style", onload: "this.onload=null;this.rel='stylesheet'", type: "text/css", media: "screen" },
+                // AOS
+                { rel: "preload", href: "https://unpkg.com/aos@next/dist/aos.css", as: "style", onload: "this.onload=null;this.rel='stylesheet'", type: "text/css", media: "screen" },
+                // Bootstrap
+                { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css", rel: "stylesheet", integrity: "sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi", crossorigin: "anonymous" },
+                { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js", integrity: "sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3", crossorigin: "anonymous" },
+            ],
         },
     },
     // ... other options
---
+++ pages/job/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        Job Pair
+        {{ title }}
+    </div>
+</template>
+<script setup lang="ts">
+// definePageMeta({
+//     title: 'Some Page'
+// })
+// let title = ref('My amazing site.')
+const { data, pending, refresh } = await useAsyncData(async (ctx) => {
+    const title = new Promise(resolve => {
+        setTimeout(() => {
+            resolve('測試職缺名稱 測試公司名稱')
+        }, 500)
+    })
+    return title
+})
+const title: any = data.value
+
+useHead({
+    title: title,
+    meta: [
+        { property: 'og:title', content: title },
+        { property: 'og:image', content: 'https://storage.googleapis.com/job-pair-taiwan-prd.appspot.com/meta/ogImageJob.png' }
+    ],
+})
+</script>
