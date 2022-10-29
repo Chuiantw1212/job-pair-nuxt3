783c4e2b50eabf70c6c745be92cf7eb589ddb89f
Ian Chu <chuiantw1212@gmail.com>
2022-10-29 22:07:19 +0800

    wip

---
+++ composables/userRepoSelect.js
@@ -0,0 +1,75 @@
+import { defineStore } from 'pinia'
+import { getAuth, } from "firebase/auth"
+export default defineStore('select', () => {
+    async function deleteUser() {
+        const auth = getAuth()
+        if (!auth.currentUser) {
+            return
+        }
+        const response = await apiDefault.request({
+            method: 'delete',
+            url: `/user`,
+        })
+        return response
+    }
+    async function postUser(data) {
+        const response = await apiDefault.request({
+            method: 'post',
+            url: `/user`,
+            data,
+            commit: true,
+        })
+        return response
+    }
+    async function patchUserProfile(data) {
+        const response = await apiDefault.request({
+            method: 'patch',
+            url: `/user`,
+            data
+        })
+        return response
+    }
+    async function putUserResume(data) {
+        const response = await apiDefault.request({
+            method: 'put',
+            url: `/user/${data.uid}/resume`,
+            data: data.file,
+            commit: true,
+        })
+        return response
+    }
+    async function putUserResumes(data) {
+        const response = await apiDefault.request({
+            method: 'put',
+            url: `/user/resumes`,
+            data,
+            commit: true,
+        })
+        return response
+    }
+    async function putUserPhoto(data) {
+        const response = await apiDefault.request({
+            method: 'put',
+            url: `/user/photo`,
+            data,
+        })
+        return response
+    }
+    async function patchUserPreference(data) {
+        const response = await apiDefault.request({
+            method: 'patch',
+            url: `/user/preference`,
+            data
+        })
+        return response
+    }
+    return {
+        deleteUser,
+        postUser,
+        patchUserProfile,
+        putUserResume,
+        putUserResumes,
+        putUserPhoto,
+        patchUserPreference,
+    }
+})
