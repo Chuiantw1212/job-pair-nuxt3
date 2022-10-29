93afa946a3135caf9b549a47c71134d142f57e13
Ian Chu <chuiantw1212@gmail.com>
2022-10-29 12:27:39 +0800

    add home

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app" class="app w-100">
+        <OrganismHeader />
+        <slot></slot>
+        <OrganismFooter />
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss">
+.app {
+    font-family: 'Noto Sans TC', sans-serif, 'charter', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif';
+    padding-top: 58px;
+    background-color: #ecf3f6;
+    overflow-x: hidden;
+}
+
+.container {
+    min-height: 100vh;
+}
+</style>
