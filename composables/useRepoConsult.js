93267677728ff095025039e33deaec22c2fa012b
Ian Chu <chuiantw1212@gmail.com>
2022-10-30 13:50:55 +0800

    add consult card

---
+++ composables/useRepoConsult.js
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { getAuth, } from "firebase/auth"
+export default defineStore('consult', () => {
+    const axios = useAxios()
+    async function getConsultants() {
+        const response = await axios.request({
+            method: 'get',
+            url: `/consult/consultant`,
+        })
+        return response
+    }
+    async function getConsultantById(id) {
+        const response = await axios.request({
+            method: 'get',
+            url: `/consult/consultant/${id}`,
+        })
+        return response
+    }
+    async function postConsultAppointment(data) {
+        const response = await axios.request({
+            method: 'post',
+            url: `/consult/appointment`,
+            data
+        })
+        return response
+    }
+    async function getConsultRecords(params) {
+        const response = await axios.request({
+            method: 'get',
+            url: `/consult/records`,
+            params,
+        })
+        return response
+    }
+    return {
+        getConsultants,
+        getConsultantById,
+        postConsultAppointment,
+        getConsultRecords
+    }
+})
