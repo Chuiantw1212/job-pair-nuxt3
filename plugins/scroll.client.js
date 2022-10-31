1c986d9a8294ebe7437133b08ce5b9fc2f865f54
Ian Chu <chuiantw1212@gmail.com>
2022-10-31 12:03:15 +0800

    add scroll plugin

---
+++ plugins/scroll.client.js
@@ -0,0 +1,24 @@
+export default defineNuxtPlugin((nuxtApp) => {
+    // https://github.com/nuxt/framework/discussions/1661
+    nuxtApp.$router.options.scrollBehavior = async (to, from, savedPosition) => {
+        let goTo
+
+        if (to.hash) {
+            goTo = {
+                el: to.hash,
+                behavior: 'smooth',
+                top: 64,
+            }
+        } else if (savedPosition) {
+            goTo = savedPosition
+        } else {
+            goTo = { top: 0, behavior: 'smooth' }
+        }
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(goTo)
+            }, 100)
+        })
+    }
+})
