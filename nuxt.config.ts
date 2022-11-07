f5ac3f8c300d7724b329e0b288c50a8f54b46fe0
Ian Chu <chuiantw1212@gmail.com>
2022-11-07 19:26:31 +0800

    update env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,11 +37,11 @@ export default defineNuxtConfig({
         '@glidejs/glide/dist/css/glide.theme.min.css',
     ],
     runtimeConfig: {
-        apiBase: 'http://localhost:8080',
+        apiBase: 'https://job-pair-taiwan-dev.de.r.appspot.com',
         public: {
             VITE_APP_ECPAY_AMOUNT: 5,
             VITE_APP_FIREBASE_ENV: 'development',
-            apiBase: 'http://localhost:8080',
+            apiBase: 'https://job-pair-taiwan-dev.de.r.appspot.com',
         }
     },
     // ... other options
