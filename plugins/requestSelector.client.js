092822436d7998aae0a20a4099068133d1c519a9
Chuian <chuiantw1212@gmail.com>
2022-11-10 11:10:30 +0800

    fix nuxt page

--- plugins/requestSelector.client.js
+++ plugins/requestSelector.client.js
@@ -2,15 +2,17 @@ export default defineNuxtPlugin(() => {
     return {
         provide: {
             requestSelector: (selectorString, callback,) => {
+                console.time(`Request element ${selectorString}`)
                 let localCount = 0
                 function step() {
                     if (localCount >= 100) {
-                        console.log(`Cannot find element ${selectorString}`)
+                        console.error(`Cannot find element ${selectorString}`)
                         return
                     }
                     const queryResult = document.querySelector(selectorString)
                     if (queryResult) {
                         callback(queryResult)
+                        console.timeEnd(`Request element ${selectorString}`)
                     } else {
                         localCount++
                         window.requestAnimationFrame(step)
