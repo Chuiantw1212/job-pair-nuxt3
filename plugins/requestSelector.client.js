721526a0f1ac3aec39dbf1e4a704f5da22927845
Chuian <chuiantw1212@gmail.com>
2022-11-12 20:45:39 +0800

    fix job scrolling

--- plugins/requestSelector.client.js
+++ plugins/requestSelector.client.js
@@ -19,6 +19,25 @@ export default defineNuxtPlugin(() => {
                     }
                 }
                 window.requestAnimationFrame(step)
+            },
+            requestSelectorAll: (selectorString, callback,) => {
+                console.time(`Request element ${selectorString}`)
+                let localCount = 0
+                function step() {
+                    if (localCount >= 100) {
+                        console.error(`Cannot find element ${selectorString}`)
+                        return
+                    }
+                    const queryResult = document.querySelectorAll(selectorString)
+                    if (queryResult) {
+                        callback(queryResult)
+                        console.timeEnd(`Request element ${selectorString}`)
+                    } else {
+                        localCount++
+                        window.requestAnimationFrame(step)
+                    }
+                }
+                window.requestAnimationFrame(step)
             }
         }
     }
