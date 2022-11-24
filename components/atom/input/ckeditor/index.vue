71d6b2612483ac038e05ffc2d95e010ef5511a2f
Chuian <chuiantw1212@gmail.com>
2022-11-24 08:57:03 +0800

    dynamic import success in local

--- components/atom/input/ckeditor/index.vue
+++ components/atom/input/ckeditor/index.vue
@@ -93,16 +93,8 @@ let localValue = computed({
 })
 // hooks
 onMounted(async () => {
-    if (process.client) {
-        const result = await import("~/assets/ckeditor5/build/ckeditor.js")
-        console.log({
-            result
-        });
-        state.id = $uuid4()
-        requestSelector(window.ClassicEditor, (ClassicEditor) => {
-            initializeCKEditor(ClassicEditor)
-        })
-    }
+    state.id = $uuid4()
+    initializeCKEditor()
 })
 onBeforeUnmount(() => {
     const ckeditorInstance = state.ckeditorInstance
@@ -139,7 +131,14 @@ function requestSelector(ClassicEditor, callback) {
     }
     step()
 }
-async function initializeCKEditor(ClassicEditor) {
+async function initializeCKEditor() {
+    if (!process.client) {
+        return
+    }
+    await import("~/assets/ckeditor5/build/ckeditor.js")
+    console.log({
+        window
+    });
     // 使用CDN
     const editorConfig = {
         toolbar: props.toolbar,
