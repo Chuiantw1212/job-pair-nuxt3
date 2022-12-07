6e9966c99695f413143a861a0137c48e2ad47044
Chuian <chuiantw1212@gmail.com>
2022-12-07 21:12:14 +0800

    fix server bug

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,18 +1,61 @@
 import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import axios from 'axios'
 const config = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
-  // Fetch all documents
+  const sitemap = new SitemapStream({ hostname: config.public.origin })
+  // works only on nuxt generate?
   const docs = await serverQueryContent(event).find()
-  const sitemap = new SitemapStream({
-    hostname: config.public.origin
-  })
   for (const doc of docs) {
+    sitemap.write({ url: doc._path, changefreq: 'monthly' })
+  }
+  // works on nuxt build
+  const staticEndpoints = getStaticEndpoints()
+  for (const staticEndpoint of staticEndpoints) {
+    sitemap.write({ url: staticEndpoint, changefreq: 'monthly' })
+  }
+  // add dynamic routing
+  const axiosInstance = axios.create({
+    baseURL: config.public.apiBase,
+    timeout: 20 * 60 * 1000,
+  })
+  const jobIdsResponse = await axiosInstance({
+    method: 'get',
+    url: '/job/ids',
+  })
+  jobIdsResponse.data.forEach((id: String) => {
     sitemap.write({
-      url: doc._path,
+      url: `/job/${id}`,
       changefreq: 'monthly'
     })
-  }
+  })
+
   sitemap.end()
   return streamToPromise(sitemap)
 })
+
+function getStaticEndpoints(): string[] {
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  const files = getFiles(`${__dirname}/../../pages`)
+  return files
+    .filter((file) => !file.includes('slug')) // exclude dynamic content
+    .map((file) => file.split('pages')[1])
+    .map((file) => {
+      return file.endsWith('index.vue') ? file.split('/index.vue')[0] : file.split('.vue')[0]
+    })
+}
+
+/**
+ * recursively get all files from /pages folder
+ */
+function getFiles(dir: string): string[] {
+  const dirents = fs.readdirSync(dir, { withFileTypes: true })
+  const files = dirents.map((dirent) => {
+    const res = resolve(dir, dirent.name)
+    return dirent.isDirectory() ? getFiles(res) : res
+  })
+  return files.flat()
+}
