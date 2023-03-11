159b7f5640144c47a5adef5338b83edcdca8b17a
Chuian <chuiantw1212@gmail.com>
2023-03-11 20:51:13 +0800

    fix again

--- pages/job/[id].vue
+++ pages/job/[id].vue
@@ -246,25 +246,21 @@ const browserConfig = computed({
 const currentInstance = getCurrentInstance()
 // hooks
 const { data: job } = await useFetch(`${runTime.apiBase}/job/${jobId.value}`, { initialCache: false })
-if (job.value) {
-    const { organizationId } = job.value
-    const { data: company } = await useFetch(`${runTime.apiBase}/company/${organizationId}`, { initialCache: false })
-    state.job = job.value
-    if (company.value) {
-        state.company = company.value
-        useHead({
-            title: () => {
-                if (job.value && company.value) {
-                    return `${job.value.name} - ${company.value.name} - Job Pair`
-                }
-            },
-            meta: [
-                { name: 'image', property: 'og:image', content: 'https://storage.googleapis.com/job-pair-taiwan-prd.appspot.com/meta/ogImageJob.png' }
-            ],
+const { organizationId } = job.value
+const { data: company } = await useFetch(`${runTime.apiBase}/company/${organizationId}`, { initialCache: false })
+state.job = job.value
+state.company = company.value
+useHead({
+    title: () => {
+        if (job?.value && company?.value) {
+            return `${job?.value?.name} - ${company?.value?.name} - Job Pair`
+        }
+    },
+    meta: [
+        { name: 'image', property: 'og:image', content: 'https://storage.googleapis.com/job-pair-taiwan-prd.appspot.com/meta/ogImageJob.png' }
+    ],
 
-        })
-    }
-}
+})
 useJsonld(() => ({
     // https://schema.org/JobPosting
     '@context': 'https://schema.org',
