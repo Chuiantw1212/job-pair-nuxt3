45f40a34b6caeee30f58d8e700420011a20974e1
Ian Chu <chuiantw1212@gmail.com>
2023-03-17 09:12:14 +0800

    add more console

--- plugins/liff.client.js
+++ plugins/liff.client.js
@@ -2,16 +2,13 @@
 // only in client-side.
 // document: https://nuxtjs.org/docs/2.x/directory-structure/plugins 
 import liff from '@line/liff'
-// export default defineNuxtPlugin((nuxtApp) => ({
-//     provide: {
-//         liff: initResult,
-//     },
-// }))
-// const emitter = mitt()
+console.log({
+    liff
+});
 export default defineNuxtPlugin(async nuxtApp => {
     const config = useRuntimeConfig()
     try {
-        const initResult = await liff.init({ liffId: config.public.liffId })
+        const initResult = await liff.init({ liffId: config.public.LIFF_ID })
         console.log({
             initResult
         })
@@ -19,8 +16,32 @@ export default defineNuxtPlugin(async nuxtApp => {
         nuxtApp.provide('liff', initResult)
     } catch (error) {
         console.log(`liff.init() failed: ${error}`);
-        if (!process.env.liffId) {
+        if (!config.public.LIFF_ID) {
             console.info('LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.');
         }
     }
 })
+// export default defineNuxtPlugin(nuxtApp => {
+//     console.log({
+//         nuxtApp
+//     });
+//     const config = useRuntimeConfig()
+//     const initResult = liff.init({ liffId: config.public.LIFF_ID })
+//         .then((test) => {
+//             console.log({
+//                 test
+//             })
+//             console.log('liff.init() done');
+//         })
+//         .catch(error => {
+//             console.log(`liff.init() failed: ${error}`);
+//             if (!config.public.LIFF_ID) {
+//                 console.info('LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable.');
+//             }
+//             return Promise.reject(error);
+//         });
+//     console.log({
+//         initResult
+//     });
+//     nuxtApp.provide('liff', initResult)
+// })
