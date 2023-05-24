782bb47158fe2b5ba03af759df793fee8addb66b
Chuian <chuiantw1212@gmail.com>
2023-05-24 22:38:44 +0800

    refine portfolio

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -8,23 +8,84 @@
                     僅能看到您的『姓名、職務類別、個人簡歷』，防止您的重要資訊外洩。
                 </div>
             </div>
-            <LazyMoleculeProfileCard name="基本資料" class="mt-3">
-                <div class="d-flex">
-                    <LazyAtomInputPhotoSingle v-model="state.profile.image" name="大頭貼"
+            <LazyMoleculeProfileCard name="基本求職資料" class="mt-3">
+                <div class="profile__basic">
+                    <LazyAtomInputPhotoSingle v-model="state.profile.image" class="basic__image" :size="'240px'"
                         @update:modelValue="uploadImage($event)">
                     </LazyAtomInputPhotoSingle>
+                    <div class="basic__contact">
+                        <LazyAtomInputText name="姓名" v-model="state.profile.name"
+                            :disabled="repoAuth.state.user && !!repoAuth.state.user.name"></LazyAtomInputText>
+                        <LazyAtomInputEmail name="Email" v-model="state.profile.email" class="mt-3" disabled>
+                        </LazyAtomInputEmail>
+                        <LazyAtomInputText name="手機號碼" v-model="state.profile.telephone" placeholder="請輸入手機" class="mt-3"
+                            required>
+                        </LazyAtomInputText>
+                    </div>
                 </div>
-                <LazyAtomInputText name="姓名" v-model="state.profile.name"
-                    :disabled="repoAuth.state.user && !!repoAuth.state.user.name"></LazyAtomInputText>
-                <LazyAtomInputEmail name="Email" v-model="state.profile.email" class="mt-3" disabled>
-                </LazyAtomInputEmail>
-                <LazyAtomInputText name="手機號碼" v-model="state.profile.telephone" placeholder="請輸入手機" class="mt-3" required>
-                </LazyAtomInputText>
+                <LazyMoleculeProfileSelectContainer v-model="state.filterOpen.occupationalCategory" name="欲申請職務類別" :max="3"
+                    class="mt-3" required>
+                    <template v-slot:header>
+                        <LazyMoleculeProfileSelectLabels v-model="state.profile.occupationalCategory" placeholder="欲申請職務類別"
+                            :items="repoSelect.jobCategory">
+                        </LazyMoleculeProfileSelectLabels>
+                    </template>
+                    <template v-slot:body>
+                        <LazyMoleculeFilterCategory v-model="state.profile.occupationalCategory"
+                            :items="repoSelect.jobCategory" :categoryMap="repoSelect.jobCategoryMap" :max="3"
+                            :isLarge="device.state.isLarge" required name="欲申請職務類別">
+                        </LazyMoleculeFilterCategory>
+                    </template>
+                </LazyMoleculeProfileSelectContainer>
+                <LazyAtomInputUploader v-model="state.profile.resumes" class="mt-3" name="履歷" :size="5242880"
+                    :accept="'.pdf'" :max="3" :required="device.state.isLarge" :getFileBuffer="getUserResume">
+                </LazyAtomInputUploader>
+                <LazyAtomInputCkeditor name="個人簡歷" v-model="state.profile.description" hint="此區塊將會揭露給企業端參考" class="mt-3"
+                    :required="state.profile.isActive" placeholder="請概述您過往的學經歷，凸顯個人優勢與專業領域，讓企業主對您留下深刻的第一印象。" :hasBtn="true"
+                    ref="description">
+                    <LazyOrganismChatGptModal v-model="state.profile.description" name="個人簡歷"
+                        :chatRequest="handleChatRequest" @update:modelValue="setDescription($event)">
+                    </LazyOrganismChatGptModal>
+                </LazyAtomInputCkeditor>
+                <div class="card__footer">
+                    <LazyAtomBtnSimple :style="{ width: '205px' }" class="mt-3" @click="handleSubmit()">儲存
+                    </LazyAtomBtnSimple>
+                </div>
+            </LazyMoleculeProfileCard>
+            <LazyMoleculeProfileCard name="進階求職資料" class="profile__information profile__doc mt-3 ">
+                <div class="profile__languageGroup">
+                    <LazyAtomInputSelect class="profile__language" v-if="repoSelect.state?.selectByQueryRes?.language"
+                        v-model="state.profile.language" name="語言能力" placeholder="選擇語言"
+                        :items="repoSelect.state.selectByQueryRes.language">
+                        <LazyOrganismLanguageModal v-if="state.profile.language === 'english'"></LazyOrganismLanguageModal>
+                    </LazyAtomInputSelect>
+                    <LazyAtomInputRadio v-if="repoSelect.state?.selectByQueryRes?.proficiency"
+                        class="languageGroup__proficiency" v-model="state.profile.proficiency"
+                        :items="repoSelect.state.selectByQueryRes.proficiency">
+                    </LazyAtomInputRadio>
+                </div>
+                <LazyAtomInputUploader class="mt-3" v-model="state.profile.certificates" :getFileBuffer="getUserCertificate"
+                    name="專業證照與能力證明">
+                </LazyAtomInputUploader>
+                <LazyMoleculePortfolio v-model="state.profile.portfolio" class="mt-3"></LazyMoleculePortfolio>
+                <div class="card__footer">
+                    <LazyAtomBtnSimple :style="{ width: '205px' }" class="mt-3" @click="handleSubmit()">儲存
+                    </LazyAtomBtnSimple>
+                </div>
+            </LazyMoleculeProfileCard>
+            <LazyMoleculeProfileCard name="精準推送" class="mt-3">
+                <LazyAtomInputCheckSingle class="information__isActive" v-model="state.profile.isActive" name="目前求職狀態">
+                    <span class="isActive__desc">若有適合的職缺，我願意讓企業主主動寄信給我</span>
+                </LazyAtomInputCheckSingle>
+                <LazyAtomInputCheckSingle class="information__isActive mt-3" v-model="state.profile.subscribed"
+                    name="EDM訂閱">
+                    <span class="isActive__desc">我願意收到EDM</span>
+                </LazyAtomInputCheckSingle>
                 <LazyAtomInputCalendar name="生日" v-model="state.profile.birthDate" class="mt-3"
-                    :disabled="repoAuth.state.user && !!repoAuth.state.user.birthDate" required>
+                    :disabled="repoAuth.state.user && !!repoAuth.state.user.birthDate">
                 </LazyAtomInputCalendar>
                 <LazyAtomInputSelect name="性別" v-model="state.profile.gender"
-                    :items="repoSelect.state.selectByQueryRes.gender" class="mt-3" required>
+                    :items="repoSelect.state.selectByQueryRes.gender" class="mt-3">
                 </LazyAtomInputSelect>
                 <LazyAtomInputSelect name="最高學位" v-model="state.profile.educationLevel"
                     :items="repoSelect.state.selectByQueryRes.educationLevel" class="mt-3">
@@ -44,66 +105,18 @@
                         </LazyMoleculeFilterCategory>
                     </template>
                 </LazyMoleculeProfileSelectContainer>
+                <div class="card__footer">
+                    <LazyAtomBtnSimple :style="{ width: '205px' }" class="mt-3" @click="handleSubmit()">儲存
+                    </LazyAtomBtnSimple>
+                </div>
+
+            </LazyMoleculeProfileCard>
+            <LazyMoleculeProfileCard name="帳號管理" class="mt-3">
                 <div class="profile__management mt-3">
-                    帳戶管理
                     <LazyOrganismDeleteModal class="managemement__others"></LazyOrganismDeleteModal>
                 </div>
-                <LazyAtomInputCheckSingle class="information__isActive mt-3" v-model="state.profile.subscribed"
-                    name="EDM訂閱">
-                    <span class="isActive__desc">我願意收到EDM</span>
-                </LazyAtomInputCheckSingle>
                 <LazyAtomBtnSimple class="mt-3" :style="{ width: '145px' }" @click="logout()">登出</LazyAtomBtnSimple>
             </LazyMoleculeProfileCard>
-            <LazyMoleculeProfileCard name="求職資訊" class="profile__information profile__doc mt-3 ">
-                <LazyAtomInputCheckSingle class="information__isActive" v-model="state.profile.isActive" name="目前求職狀態">
-                    <span class="isActive__desc">若有適合的職缺，我願意讓企業主主動寄信給我</span>
-                </LazyAtomInputCheckSingle>
-                <LazyMoleculeProfileSelectContainer v-model="state.filterOpen.occupationalCategory" name="欲申請職務類別"
-                    class="mt-4" :max="3" required>
-                    <template v-slot:header>
-                        <LazyMoleculeProfileSelectLabels v-model="state.profile.occupationalCategory" placeholder="欲申請職務類別"
-                            :items="repoSelect.jobCategory">
-                        </LazyMoleculeProfileSelectLabels>
-                    </template>
-                    <template v-slot:body>
-                        <LazyMoleculeFilterCategory v-model="state.profile.occupationalCategory"
-                            :items="repoSelect.jobCategory" :categoryMap="repoSelect.jobCategoryMap" :max="3"
-                            :isLarge="device.state.isLarge" required name="欲申請職務類別">
-                        </LazyMoleculeFilterCategory>
-                    </template>
-                </LazyMoleculeProfileSelectContainer>
-                <div class="profile__languageGroup mt-4">
-                    <LazyAtomInputSelect class="profile__language" v-if="repoSelect.state?.selectByQueryRes?.language"
-                        v-model="state.profile.language" name="語言能力" placeholder="選擇語言"
-                        :items="repoSelect.state.selectByQueryRes.language">
-                        <LazyOrganismLanguageModal v-if="state.profile.language === 'english'"></LazyOrganismLanguageModal>
-                    </LazyAtomInputSelect>
-                    <LazyAtomInputRadio v-if="repoSelect.state?.selectByQueryRes?.proficiency"
-                        class="languageGroup__proficiency" v-model="state.profile.proficiency"
-                        :items="repoSelect.state.selectByQueryRes.proficiency">
-                    </LazyAtomInputRadio>
-                </div>
-                <LazyAtomInputUploader class="mt-3" v-model="state.profile.certificates" :getFileBuffer="getUserCertificate"
-                    name="證照與證明">
-                </LazyAtomInputUploader>
-                <LazyAtomInputCkeditor name="個人簡歷" v-model="state.profile.description" hint="此區塊將會揭露給企業端參考"
-                    class="resume__introduction mt-3" :required="state.profile.isActive"
-                    placeholder="請概述您過往的學經歷，凸顯個人優勢與專業領域，讓企業主對您留下深刻的第一印象。" :hasBtn="true" ref="description">
-                    <LazyOrganismChatGptModal v-model="state.profile.description" name="個人簡歷"
-                        :chatRequest="handleChatRequest" @update:modelValue="setDescription($event)">
-                    </LazyOrganismChatGptModal>
-                </LazyAtomInputCkeditor>
-            </LazyMoleculeProfileCard>
-            <LazyMoleculeProfileCard name="履歷作品集" class="profile__information profile__doc mt-3 ">
-                <LazyAtomInputUploader v-model="state.profile.resumes" name="履歷" :size="5242880" :accept="'.pdf'" :max="3"
-                    :required="device.state.isLarge" :getFileBuffer="getUserResume">
-                </LazyAtomInputUploader>
-                <LazyMoleculePortfolio v-model="state.profile.portfolio"></LazyMoleculePortfolio>
-            </LazyMoleculeProfileCard>
-            <div class="profile__footer">
-                <LazyAtomBtnSimple :style="{ width: '205px' }" class="mt-3" @click="handleSubmit()">儲存
-                </LazyAtomBtnSimple>
-            </div>
         </template>
     </div>
 </template>
@@ -241,6 +254,13 @@ async function handleSubmit() {
 </script>
 <style lang="scss" scoped>
 .profile {
+    .profile__basic {
+        .basic__image {
+            display: flex;
+            justify-content: center;
+        }
+    }
+
     .profile__hint {
         background-color: #d8e4eb;
         padding: 40px 24px;
@@ -297,10 +317,29 @@ async function handleSubmit() {
         display: flex;
         justify-content: center;
     }
+
+    .card__footer {
+        display: flex;
+        justify-content: center;
+    }
 }
 
 @media screen and (min-width: 992px) {
     .profile {
+
+        .profile__basic {
+            display: flex;
+            gap: 32px;
+
+            .basic__image {
+                display: flex;
+                justify-content: center;
+            }
+
+            .basic__contact {
+                width: 100%;
+            }
+        }
 
         .profile__language {
             min-width: 232px;
