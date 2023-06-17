838a3a41a8afb094a3f03d4b0a6074b61ce546ed
Ian Chu <chuiantw1212@gmail.com>
2023-06-17 11:04:00 +0800

    fix bug

--- pages/admin/manage/company.vue
+++ pages/admin/manage/company.vue
@@ -4,7 +4,7 @@
 <script setup>
 const runTimeConfig = useRuntimeConfig()
 useSeoMeta({
-    title: () => `企業檔案 - 招募中心 - ${runTimeConfig.public.title}`,
-    ogTitle: () => `企業檔案 - 招募中心 - ${runTimeConfig.public.title}`
+    title: () => `企業檔案 - 管理中心 - ${runTimeConfig.public.title}`,
+    ogTitle: () => `企業檔案 - 管理中心 - ${runTimeConfig.public.title}`
 })
 </script>
