e9820fb843a324f5770ceb89d42930a306f0602c
Ian Chu <chuiantw1212@gmail.com>
2023-06-19 16:16:19 +0800

    fix alt

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,9 @@
         <div class="admin__cardGroup">
             <div class="cardGroup__header">Job Pair 與 一般求職平台比較</div>
             <img v-if="device.state.isLarge" class="cardGroup__decoration cardGroup__decoration--1"
-                src="@/assets/admin/index/bg2.svg">
+                src="@/assets/admin/index/bg2.svg" alt="decoration">
             <img v-if="device.state.isLarge" class="cardGroup__decoration cardGroup__decoration--2"
-                src="@/assets/admin/index/bg3.svg">
+                src="@/assets/admin/index/bg3.svg" alt="decoration">
             <div class="cardGroup__body">
                 <div v-if="device.state.isLarge" class="body__title">
                     <div class="title__others">
@@ -29,7 +29,7 @@
             </div>
         </div>
         <div class="admin__definition">
-            <img v-if="device.state.isLarge" class="definition__round" src="@/assets/index/round.png">
+            <img v-if="device.state.isLarge" class="definition__round" src="@/assets/index/round.png" alt="background">
             <div class="definition__textGroup">
                 <div class="definition__title">
                     什麼是適配度？
