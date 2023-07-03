0d73778d912c540aa131847ca6ebe3afb00f3bd3
Ian Chu <chuiantw1212@gmail.com>
2023-07-03 14:38:07 +0800

    update style

--- pages/admin/manage/topup.vue
+++ pages/admin/manage/topup.vue
@@ -25,7 +25,54 @@
                 <li class="list__item">
                     <div>
                         <img src="@/assets/admin/topup/Vector.svg" />
-                        刊登筆數無上限
+                        刊登筆數1筆
+                    </div>
+                </li>
+                <!-- <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        品牌0到1
+                    </div>
+                </li> -->
+            </ul>
+            <LazyAtomBtnSimple class="btnSimple--outline--success card__btn">去看看</LazyAtomBtnSimple>
+        </div>
+        <div class="topup__card">
+            <div class="card__header">
+                <img src="@/assets/admin/topup/885.png" />
+                <div class="header__textGroup">
+                    <div class="textGroup__largeText">
+                        10點 <span class="largeText__hightlight">省下 5％ 的費用</span>
+                    </div>
+                    <div class="textGroup__smallText">
+                        NT$ <span class="smallText__hightlight">1,000</span>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <ul class="card__list">
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        刊登筆數1筆
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        人才搜尋
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        招募課程
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        招募面談技巧
                     </div>
                 </li>
                 <li class="list__item">
@@ -34,17 +81,102 @@
                         品牌0到1
                     </div>
                 </li>
-                <!-- <li></li> -->
             </ul>
+            <LazyAtomBtnSimple class="btnSimple--outline--success card__btn">去看看</LazyAtomBtnSimple>
         </div>
         <div class="topup__card">
-            <img src="@/assets/admin/topup/841.png" />
+            <div class="card__header">
+                <img src="@/assets/admin/topup/841.png" />
+                <div class="header__textGroup">
+                    <div class="textGroup__largeText">
+                        50點 <span class="largeText__hightlight">省下 5％ 的費用</span>
+                    </div>
+                    <div class="textGroup__smallText">
+                        NT$ <span class="smallText__hightlight">4,700</span>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <ul class="card__list">
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        刊登筆數1筆
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        人才搜尋
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        招募課程
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        招募面談技巧
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        品牌0到1
+                    </div>
+                </li>
+            </ul>
+            <LazyAtomBtnSimple class="btnSimple--outline--success card__btn">去看看</LazyAtomBtnSimple>
         </div>
         <div class="topup__card">
-            <img src="@/assets/admin/topup/881.png" />
-        </div>
-        <div class="topup__card">
-            <img src="@/assets/admin/topup/885.png" />
+            <div class="card__header">
+                <img src="@/assets/admin/topup/881.png" />
+                <div class="header__textGroup">
+                    <div class="textGroup__largeText">
+                        100點 <span class="largeText__hightlight">省下 10％ 的費用</span>
+                    </div>
+                    <div class="textGroup__smallText">
+                        NT$ <span class="smallText__hightlight">9,000</span>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <ul class="card__list">
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        刊登筆數1筆
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        人才搜尋
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        招募課程
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        招募面談技巧
+                    </div>
+                </li>
+                <li class="list__item">
+                    <div>
+                        <img src="@/assets/admin/topup/Vector.svg" />
+                        品牌0到1
+                    </div>
+                </li>
+            </ul>
+            <LazyAtomBtnSimple class="btnSimple--outline--success card__btn">去看看</LazyAtomBtnSimple>
         </div>
     </div>
 </template>
@@ -72,6 +204,7 @@
         background-color: #fff;
         margin-top: 20px;
         border-radius: 7px 7px 0 0;
+        text-align: center;
     }
 
     .topup__card {
@@ -95,6 +228,17 @@
                     letter-spacing: normal;
                     text-align: left;
                     color: #333;
+
+                    .largeText__hightlight {
+                        font-size: 13px;
+                        font-weight: bold;
+                        font-stretch: normal;
+                        font-style: normal;
+                        line-height: normal;
+                        letter-spacing: normal;
+                        text-align: left;
+                        color: #5ea88e;
+                    }
                 }
 
                 .textGroup__smallText {
@@ -106,6 +250,17 @@
                     letter-spacing: normal;
                     text-align: left;
                     color: #333;
+
+                    .smallText__hightlight {
+                        font-size: 24px;
+                        font-weight: bold;
+                        font-stretch: normal;
+                        font-style: normal;
+                        line-height: normal;
+                        letter-spacing: normal;
+                        text-align: left;
+                        color: #333;
+                    }
                 }
             }
         }
@@ -114,6 +269,10 @@
             list-style: none;
             width: fit-content;
             margin: auto;
+        }
+
+        .card__btn {
+            margin-top: 20px;
         }
     }
 }
