f9321f5c8307bfc3d9d2d2de9551626e74dbfc8a
Chuian <chuiantw1212@gmail.com>
2023-07-18 22:21:09 +0800

    fix share button

--- components/organism/JobItemPanel/index.vue
+++ components/organism/JobItemPanel/index.vue
@@ -44,13 +44,8 @@
                 <img class="share__icon" src="./share.svg" alt="share" />分享
             </LazyAtomBtnSimple>
             <template v-if="showShareButton && !state.navigator.share">
-                <LazyAtomBtnSimple v-show="state.isCopied" :id="`copied${state.id}`" class="panel__share"
-                    data-bs-toggle="tooltip" :title="state.copiedTitle" @mouseout="resetCopiedTooltip()">
-                    <img class="share__icon" src="./share.svg" alt="share" />
-                    分享
-                </LazyAtomBtnSimple>
-                <LazyAtomBtnSimple v-show="!state.isCopied" :id="`tooltip${state.id}`" class="panel__share"
-                    data-bs-toggle="tooltip" :title="state.shareButtonTitle" @click="shareLinkBootstrap()">
+                <LazyAtomBtnSimple :id="`tooltip-${state.id}`" class="panel__share" data-bs-toggle="tooltip"
+                    :title="state.shareButtonTitle" @click="shareLinkBootstrap()">
                     <img class="share__icon" src="./share.svg" alt="share" />
                     分享
                 </LazyAtomBtnSimple>
@@ -65,14 +60,12 @@ export default {
 </script>
 <script setup>
 import { nextTick, } from 'vue'
-const { $uuid4, $bootstrap, $emitter, $rank, $requestSelector } = useNuxtApp()
+const { $uuid4, $emitter, $rank, $requestSelector } = useNuxtApp()
 const repoAuth = useRepoAuth()
 const repoJobApplication = useRepoJobApplication()
 const route = useRoute()
 const state = reactive({
     id: null,
-    isCopied: false,
-    copiedTooltip: null,
     application: {},
     navigator: {},
     shareButtonToolTip: null,
@@ -106,12 +99,7 @@ watch(() => props.modelValue, () => {
         const { origin } = window.location
         const url = `${origin}/job/${props.modelValue.identifier}`
         state.copiedTitle = `已複製: ${url}`
-        if (props.showShareButton && !state.navigator.share) {
-            state.id = $uuid4()
-            nextTick(() => {
-                initialilzeTooltip()
-            })
-        }
+        initialilzeTooltip()
     }
 }, { immediate: true })
 watch(() => repoJobApplication.state.userJobs, (userJobs) => {
@@ -150,16 +138,16 @@ function checkPanelDisplay() {
     const { user } = repoAuth.state
     return user?.id
 }
-function resetCopiedTooltip() {
-    state.isCopied = false
-}
 function initialilzeTooltip() {
-    $requestSelector(`#tooltip${state.id}`, (element) => {
-        state.shareButtonToolTip = new window.bootstrap.Tooltip(element)
-    })
-    $requestSelector(`#copied${state.id}`, (element) => {
-        state.copiedTooltip = new window.bootstrap.Tooltip(element)
-    })
+    if (props.showShareButton && !state.navigator.share) {
+        state.id = $uuid4()
+        nextTick(() => {
+            const element = document.querySelector(`#tooltip-${state.id}`)
+            if (element) {
+                state.shareButtonToolTip = new window.bootstrap.Tooltip(element)
+            }
+        })
+    }
 }
 async function handleUnsavedJob() {
     const { user } = repoAuth.state
@@ -200,13 +188,7 @@ async function shareLinkBootstrap() {
     const { origin } = window.location
     const url = `${origin}/job/${props.modelValue.identifier}?openExternalBrowser=1`
     await navigator.clipboard.writeText(url)
-    if (state.shareButtonToolTip) {
-        state.shareButtonToolTip.hide()
-    }
-    state.isCopied = true
-    nextTick(() => {
-        state.copiedTooltip.show()
-    })
+    state.shareButtonToolTip.hide()
 }
 </script>
 <style lang="scss" scoped>
