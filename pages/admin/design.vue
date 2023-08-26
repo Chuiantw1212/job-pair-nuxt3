717c7b1440ead69d8755169516890ea4e28f32ec
Chuian <chuiantw1212@gmail.com>
2023-08-26 20:18:38 +0800

    add design page

---
+++ pages/admin/design.vue
@@ -0,0 +1,197 @@
+<template>
+    <div class="design">
+        <div class="design__body">
+            <div class="body__category">
+                <!-- <AtomBtnSimple class="category__goback">上一頁</AtomBtnSimple> -->
+                <div class="category__color">
+                    <div class="color__titleGroup">
+                        <div class="titleGroup__title">
+                            主色調
+                        </div>
+                        <div class="titleGroup__desc">
+                            可調整按鈕以及大標題顏色
+                        </div>
+                    </div>
+                    <LazyAtomInputColor v-model="state.color"></LazyAtomInputColor>
+                </div>
+                <hr class="category__line">
+                <div class="category__blocks">
+                    <div class="blocks__title">
+                        選擇布局
+                    </div>
+                    <ul class="blocks__list">
+                        <li class="list__item">
+                            <img class="item__imaage"
+                                src="https://storage.googleapis.com/public.prd.job-pair.com/meta/companyBanner.png">
+                            <div class="item__desc">適合 Banner ，大圖襯底，大標、副標和按鈕</div>
+                        </li>
+                        <li class="list__item">
+                            <img class="item__imaage"
+                                src="https://storage.googleapis.com/public.prd.job-pair.com/meta/companyBanner.png">
+                            <div class="item__desc">適合 Banner ，大圖襯底，大標、副標和按鈕</div>
+                        </li>
+                        <li class="list__item">
+                            <img class="item__imaage"
+                                src="https://storage.googleapis.com/public.prd.job-pair.com/meta/companyBanner.png">
+                            <div class="item__desc">適合 Banner ，大圖襯底，大標、副標和按鈕</div>
+                        </li>
+                        <li class="list__item">
+                            <img class="item__imaage"
+                                src="https://storage.googleapis.com/public.prd.job-pair.com/meta/companyBanner.png">
+                            <div class="item__desc">適合 Banner ，大圖襯底，大標、副標和按鈕</div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="body__preview">
+                <MoleculeDesignBanner class="preview__item"></MoleculeDesignBanner>
+                <!-- <img class="preview__image"
+                    src="https://storage.googleapis.com/public.prd.job-pair.com/meta/companyBanner.png"> -->
+                <div class="preview__template">
+                    請先點擊區域，再選擇模板
+                </div>
+            </div>
+        </div>
+        <div class="design__footer">
+            <div class="footer__desc">
+                目前的方案可使用五個區域
+            </div>
+            <AtomBtnSimple class="footer_btn">存為草稿</AtomBtnSimple>
+            <AtomBtnSimple class="footer_btn">發布頁面</AtomBtnSimple>
+            <AtomBtnSimple class="footer_btn">預覽頁面</AtomBtnSimple>
+        </div>
+    </div>
+</template>
+<script setup>
+const state = reactive({
+    color: '#5EA88E',
+})
+</script>
+<style lang="scss" scoped>
+.design {
+    background-color: white;
+
+    .design__body {
+        display: flex;
+
+        .body__category {
+            width: 30%;
+            border-right: solid 1px #d3d3d3;
+            height: 100%;
+            // padding: 20px 20px 0 20px;
+
+            .category__goback {
+                width: 115px;
+                height: 40px;
+            }
+
+            .category__color {
+                display: flex;
+                justify-content: space-between;
+                // margin-top: 30px;
+                padding: 20px 20px 20px 20px;
+
+                .color__titleGroup {
+                    .titleGroup__title {
+                        font-size: 20px;
+                        font-weight: bold;
+                    }
+
+                    .titleGroup__desc {
+                        font-size: 16px;
+                        font-weight: normal;
+                    }
+                }
+            }
+
+            .category__line {
+                margin: 0;
+            }
+
+            .category__blocks {
+                font-size: 20px;
+                font-weight: bold;
+                padding: 20px 0px 0px 0px;
+
+                .blocks__title {
+                    padding: 0px 20px;
+                }
+
+                .blocks__list {
+                    list-style: none;
+                    padding: 0px;
+                    margin-top: 20px;
+                    margin-bottom: 0px;
+                    max-height: calc(100vh - 225px);
+                    overflow-y: auto;
+                    padding: 0px 20px;
+
+                    .list__item {
+                        border-top: solid 1px #d3d3d3;
+                        font-size: 16px;
+                        font-weight: normal;
+                        padding: 20px 0px;
+
+                        .item__imaage {
+                            display: block;
+                            max-width: 100%;
+                        }
+
+                        .item__desc {
+                            margin-top: 10px;
+                        }
+                    }
+                }
+            }
+        }
+
+        .body__preview {
+            width: 70%;
+            padding: 20px;
+
+            .preview__item {
+
+                width: 100%;
+            }
+
+            .preview__template {
+                border: dashed 1px #5ea88e;
+                background-color: rgba(94, 168, 142, 0.1);
+                max-width: 100%;
+                height: 313px;
+                margin-top: 20px;
+                font-size: 20px;
+                line-height: 313px;
+                text-align: center;
+            }
+        }
+    }
+
+    .design__footer {
+        font-size: 20px;
+        display: flex;
+        justify-content: flex-end;
+        gap: 30px;
+        padding: 20px 50px;
+        border-top: solid 1px #d3d3d3;
+        align-items: center;
+
+        .footer__desc {
+            font-size: 20px;
+            font-weight: normal;
+            font-stretch: normal;
+            font-style: normal;
+            line-height: 1.5;
+            letter-spacing: normal;
+            text-align: left;
+            color: #d60b00;
+        }
+
+        .footer_btn {
+            width: 140px;
+            height: 60px;
+        }
+    }
+
+}
+</style>
