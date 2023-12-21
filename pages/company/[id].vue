67e402903361d6cf359c63c57a704ee70883f3d7
Chuian <chuiantw1212@gmail.com>
2023-12-21 23:08:51 +0800

    fix scroll

--- pages/company/[id].vue
+++ pages/company/[id].vue
@@ -184,11 +184,13 @@ useJsonld(() => ({
 onMounted(async () => {
     state.id = $uuid4()
     await initializeCompany(paramdId.value)
+    $emitter.on('scrollToJobs', scrollToJobs)
 })
 onBeforeUnmount(() => {
     if (state.glideInstance) {
         state.glideInstance.destroy()
     }
+    $emitter.off('scrollToJobs', scrollToJobs)
 })
 watch(() => repoAuth.state.user, () => {
     jobScroller.state.filter.organizationId = company.value.organizationId || company.value.id
@@ -201,6 +203,12 @@ watch(() => jobScroller.state.jobList, (newValue = [], oldValue = []) => {
     }
 })
 // methods
+function scrollToJobs() {
+    const element = document.getElementById('company__jobs')
+    if (element) {
+        element.scrollIntoView()
+    }
+}
 function extractContent(content = '') {
     const target = content.replaceAll("<[^>]*>", "");
     return target
