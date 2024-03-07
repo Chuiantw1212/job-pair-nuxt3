a97dd28c3b6625bbaa6eb1b6fb1ef5445812f1ff
Chuian <chuiantw1212@gmail.com>
2024-03-07 23:16:34 +0800

    remove check

--- server/api/sitemap.ts
+++ server/api/sitemap.ts
@@ -4,39 +4,35 @@ export default defineSitemapEventHandler(async (e) => {
         month: '2-digit',
         day: '2-digit',
     })
-    
+
     const [jobIdsResponse, companyIdsResponse]: any[] = await Promise.all([
         $fetch('http://localhost:8080/job/sitemap'),
         $fetch('http://localhost:8080/company/sitemap'),
     ])
     const urls: { loc: string, lastMod: string, }[] = []
-    if (jobIdsResponse?.length) {
-        jobIdsResponse.forEach((item: { identifier: string, datePosted: string }) => {
-            const urlItem: any = {
-                loc: `/job/${item.identifier}`,
-                lastmod: "",
-            }
-            const { datePosted = '' } = item
-            if (datePosted) {
-                const dateInstance = new Date(datePosted)
-                urlItem.lastmod = formatter.format(dateInstance)
-            }
-            urls.push(urlItem)
-        })
-    }
-    if (companyIdsResponse?.length) {
-        companyIdsResponse.forEach((item: { identifier: string, updatedDate: string }) => {
-            const urlItem: any = {
-                loc: `/job/${item.identifier}`,
-                lastmod: "",
-            }
-            const { updatedDate = '' } = item
-            if (updatedDate) {
-                const dateInstance = new Date(updatedDate)
-                urlItem.lastmod = formatter.format(dateInstance)
-            }
-            urls.push(urlItem)
-        })
-    }
+    jobIdsResponse.forEach((item: { identifier: string, datePosted: string }) => {
+        const urlItem: any = {
+            loc: `/job/${item.identifier}`,
+            lastmod: "",
+        }
+        const { datePosted = '' } = item
+        if (datePosted) {
+            const dateInstance = new Date(datePosted)
+            urlItem.lastmod = formatter.format(dateInstance)
+        }
+        urls.push(urlItem)
+    })
+    companyIdsResponse.forEach((item: { identifier: string, updatedDate: string }) => {
+        const urlItem: any = {
+            loc: `/job/${item.identifier}`,
+            lastmod: "",
+        }
+        const { updatedDate = '' } = item
+        if (updatedDate) {
+            const dateInstance = new Date(updatedDate)
+            urlItem.lastmod = formatter.format(dateInstance)
+        }
+        urls.push(urlItem)
+    })
     return urls
 });
