e44c29d974213df67fbe645e385ee92a91d9a09c
Chuian <chuiantw1212@gmail.com>
2024-03-16 22:31:22 +0800

    wip

--- pages/admin/manage/topup.vue
+++ pages/admin/manage/topup.vue
@@ -26,7 +26,8 @@
                         <img class="item__img" src="@/assets/admin/topup/Check.svg">應徵人選適配度
                     </li>
                 </ul>
-                <LazyAtomBtnSimpleV2 class="card__btn" outline>訂閱</LazyAtomBtnSimpleV2>
+                <LazyAtomBtnSimpleV2 class="card__btn" outline @click="submitAppointment(subscription[0])">訂閱
+                </LazyAtomBtnSimpleV2>
             </div>
             <div class="topup__card">
                 <div class="card__title">{{ subscription[1]?.text }}</div>
@@ -49,7 +50,8 @@
                         微型企業，渴望打造具有獨特風格的企業形象頁面，以吸引客戶和潛在人才。
                     </li>
                 </ul>
-                <LazyAtomBtnSimpleV2 class="card__btn" outline>訂閱</LazyAtomBtnSimpleV2>
+                <LazyAtomBtnSimpleV2 class="card__btn" outline @click="submitAppointment(subscription[1])">訂閱
+                </LazyAtomBtnSimpleV2>
             </div>
             <div class="topup__card">
                 <div class="card__title">{{ subscription[2]?.text }}</div>
@@ -78,7 +80,8 @@
                         一般企業，建立具有專業企業文化風格的形象頁面，加強僱主品牌，並主動聯繫潛在合適人選，擴大企業影響力。
                     </li>
                 </ul>
-                <LazyAtomBtnSimpleV2 class="card__btn" outline>訂閱</LazyAtomBtnSimpleV2>
+                <LazyAtomBtnSimpleV2 class="card__btn" outline @click="submitAppointment(subscription[2])">訂閱
+                </LazyAtomBtnSimpleV2>
             </div>
             <div class="topup__card">
                 <div class="card__title">{{ subscription[3]?.text }}</div>
@@ -107,7 +110,8 @@
                         中大型企業，希望透過客製化頁面和廣泛的潛在人選邀請，更有效地推廣企業形象和吸引人才。
                     </li>
                 </ul>
-                <LazyAtomBtnSimpleV2 class="card__btn" outline>訂閱</LazyAtomBtnSimpleV2>
+                <LazyAtomBtnSimpleV2 class="card__btn" outline @click="submitAppointment(subscription[3])">訂閱
+                </LazyAtomBtnSimpleV2>
             </div>
         </div>
     </div>
@@ -121,6 +125,32 @@ console.log(repoSelect);
 const state = reactive({
     test: ''
 })
+// methods
+async function submitAppointment(subscription) {
+    const result = await $validate()
+    if (!result.isValid) {
+        return
+    }
+    state.appointmentForm.userId = repoAuth.state.user.id
+    state.appointmentForm.amount = subscription.price
+    if (repoAuth.state?.user?.email === 'ian.chu@job-pair.com') {
+        state.appointmentForm.amount = 5
+    }
+    // const response = await repoConsult.postConsultAppointment(state.appointmentForm)
+    // if (response.status !== 200) {
+    //     state.consultants.forEach(item => {
+    //         item.key = Math.random()
+    //     })
+    //     state.appointmentForm = {
+    //         consultantId,
+    //         service: service,
+    //         question,
+    //         time: {}, // 重新設定時段就好
+    //     }
+    //     return
+    // }
+    // document.write(response.data)
+}
 </script>
 <style lang=scss scoped>
 .topup {
