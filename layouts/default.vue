dee2927f16e3c380a9f6d5149e42cdd061c9c6f3
ian-chu-job-pair <162425644+en-job-pair@users.noreply.github.com>
2024-03-29 11:25:16 +0800

    update font

--- layouts/default.vue
+++ layouts/default.vue
@@ -68,7 +68,7 @@ useSchemaOrg([
 <style lang="scss" scoped>
 .app {
     font-display: swap;
-    font-family: -apple-system, BlinkMacSystemFont, 'Noto Sans TC', Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, 'Noto Sans TC', sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
     background-color: #f9f9f9;
     overflow-x: hidden;
 }
