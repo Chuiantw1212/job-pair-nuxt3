7eeffbe8e068351f7ebcbbb0f22c6f2b71e44181
Chuian <chuiantw1212@gmail.com>
2024-04-05 09:43:52 +0800

    update menu mobule

--- pages/admin/manage.vue
+++ pages/admin/manage.vue
@@ -40,10 +40,8 @@ definePageMeta({
     }
 
     .dashboard__menu {
-
-
         .menu__content {
-            background-color: #f8f9fa;
+            background-color: #FFF;
             display: flex;
             list-style-type: none;
             padding: 0;
@@ -53,17 +51,23 @@ definePageMeta({
             bottom: 0;
             z-index: 1040;
             margin-bottom: 0;
-            border-top: 1px solid #c4c4c4;
+            border-top: 1px solid #EDEAEA;
             box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.1);
+            height: 57px;
 
             .menu__item {
+                height: 100%;
+                width: 100%;
+
                 .menu__item__link {
-                    display: block;
-                    padding: 6px 12px;
+                    justify-content: center;
+
+                    display: flex;
+                    align-items: center;
+                    height: 100%;
                     text-decoration: none;
                     color: #6c6c6c;
                     transition: all 0.3s;
-                    border: 2px solid rgba(0, 0, 0, 0);
                     text-align: center;
 
                     .item__link__icon {
@@ -74,7 +78,6 @@ definePageMeta({
 
                     &:hover {
                         color: #5ea88e;
-                        border-bottom: 3px solid #5ea88e;
 
                         .item__link__icon {
                             filter: grayscale(0);
@@ -84,29 +87,18 @@ definePageMeta({
 
                 .menu__item__link--active {
                     color: #5ea88e;
-                    border-bottom: 3px solid #5ea88e;
-
-                    // .link__icon {
-                    //     margin-top: 4px;
-                    //     display: flex;
-                    // }
 
                     .item__link__icon {
                         filter: grayscale(0);
                     }
                 }
-
             }
         }
-
-
     }
 }
 
 @media screen and (min-width: 992px) {
     .dashboard {
-
-
         .dashboard__menu {
             min-height: 64px;
 
@@ -123,11 +115,6 @@ definePageMeta({
                         display: flex;
                         align-items: center;
                         padding: 18px 0;
-
-                        // .link__icon {
-                        //     margin-top: 0;
-                        //     margin-left: 8px;
-                        // }
                     }
 
                 }
