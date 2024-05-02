663ecb5e520034fd179578822a4250f214498ff4
Chuian <chuiantw1212@gmail.com>
2024-05-02 18:44:44 +0800

    update save

--- components/organism/CompanyProfile/index.vue
+++ components/organism/CompanyProfile/index.vue
@@ -111,9 +111,6 @@
                 <NuxtLink v-else class="footerGroup__submit" target="_blank" :to="`/company/${state.companyInfo.id}`">
                     檢視公司頁面
                 </NuxtLink>
-                <button class="footerGroup__submit" type="button" @click="saveCompanyInfo({ validate: true })">
-                    儲存
-                </button>
             </template>
         </div>
     </div>
