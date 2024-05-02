86181b91174cbca583b8f45f877629c85c68d0ef
Chuian <chuiantw1212@gmail.com>
2024-05-02 16:31:42 +0800

    new culture completed

--- pages/admin/manage/preference.vue
+++ pages/admin/manage/preference.vue
@@ -1,19 +1,45 @@
 <template>
     <div class="preference">
-        <h1 class="preference__header">企業檔案</h1>
+        <h1 class="preference__header">企業風格文化</h1>
         <hr />
         <div class="preference__card">
-            <ul>
-                <!-- v-model="state.companyInfo.preference.culture" required :items="repoSelect.state.questionsRes[5].items" -->
-                <li>充滿競爭</li>
-                <li></li>
-                <li></li>
+            <h2 class="card__header">選擇企業領導偏好</h2>
+            <div class="card__desc">可選 2 個選項</div>
+            <ul class="card__list">
+                <li class="list__item" v-for="(item, index) in cultureItems" :key="index">
+                    <label class="item__label"
+                        :class="{ 'item__label--isSelected': checkIsSelected(item.value), 'item__label--disabled': checkDisabled(item.value) }"
+                        :for="item.value">
+                        {{ item.textCompany }}
+                        <input v-show="false" type="checkbox" :id="item.value" :value="item.value"
+                            v-model="checkedCultures" :disabled="checkDisabled(item.value)">
+                    </label>
+                </li>
             </ul>
         </div>
     </div>
 </template>
 <script setup>
 const repoSelect = useRepoSelect()
+const cultureItems = computed(() => {
+    if (repoSelect.state.questionsRes?.length) {
+        return repoSelect.state.questionsRes[5].items
+    } else {
+        return []
+    }
+})
+const checkedCultures = ref([])
+const props = defineProps({
+
+})
+function checkIsSelected(value) {
+    return checkedCultures.value.includes(value)
+}
+function checkDisabled(value) {
+    const isMax = checkedCultures.value.length === 2
+    const isNotSelected = !checkedCultures.value.includes(value)
+    return isMax && isNotSelected
+}
 </script>
 <style lang="scss" scoped>
 .preference {
@@ -34,6 +60,63 @@ const repoSelect = useRepoSelect()
     .preference__card {
         border-radius: 10px;
         background: var(--Grays-Quin, #FFF);
+        padding: 20px;
+
+        .card__header {
+            color: var(--Grays-Prim, #222);
+            /* H2-24-Medium */
+            font-family: "PingFang TC";
+            font-size: 24px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+        }
+
+        .card__desc {
+            color: var(--Grays-Tert, #A6A6A6);
+            font-family: "PingFang TC";
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: normal;
+        }
+
+        .card__list {
+            margin: 0px;
+            padding: 0px;
+            list-style: none;
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            margin-top: 10px;
+            width: 100%;
+
+            .list__item {
+                width: 100%;
+
+                .item__label {
+                    padding: 10px 20px;
+                    color: var(--Grays-Prim, #222);
+                    font-family: "PingFang TC";
+                    font-size: 12px;
+                    font-style: normal;
+                    font-weight: 500;
+                    line-height: normal;
+                    background: inherit;
+                    border-radius: 100px;
+                    border: 1px solid var(--Grays-Quat, #EDEAEA);
+                    width: 100%;
+                }
+
+                .item__label--isSelected {
+                    background-color: #D3F5E9;
+                }
+
+                .item__label--disabled {
+                    background-color: rgb(239, 239, 239);
+                }
+            }
+        }
     }
 }
 </style>
