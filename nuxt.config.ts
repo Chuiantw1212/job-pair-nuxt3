d5585eec25ee4838799b399c1d481db0c1f0ce86
Chuian <chuiantw1212@gmail.com>
2024-05-14 21:06:23 +0800

    debug prd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const apiBase = 'https://job-pair-taiwan-prd.de.r.appspot.com'
-const siteUrl = 'https://job-pair.com'
+const apiBase = 'http://localhost:8080'
+const siteUrl = 'http://localhost:3000'
 // SEO
 const imageUrl = 'https://storage.googleapis.com/public.prd.job-pair.com/meta/ogImageJob.png'
 export default defineNuxtConfig({
@@ -54,12 +54,11 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       VITE_APP_ECPAY_AMOUNT: 2000,
-      VITE_APP_FIREBASE_ENV: 'production',
+      VITE_APP_FIREBASE_ENV: 'development',
       apiBase, // deprecated
       siteUrl,
       siteDescription: 'Job Pair 是專業的媒合平台，專注於助您找到與能力、價值觀相符的工作機會，實現長期價值發揮。透過我們高效的適配功能，輕鬆探索理想職位。立即加入 Job Pair，啟動您的職業生涯。',
       language: 'zh-TW', // prefer more explicit language codes like `en-AU` over `en`
-      LIFF_ID: '1660783051-vP4Ojz2r',
       axiosTimeout: 30000,
     }
   },
