8da080b0c16fdec8a5358fe9c98113411e89d135
Chuian <chuiantw1212@gmail.com>
2024-07-15 21:35:00 +0800

    add patch

--- pages/job/[id].vue
+++ pages/job/[id].vue
@@ -662,6 +662,12 @@ async function initialize() {
     const job = jobResponse.data
     // 再取得公司資料
     const companyResponse = await repoCompany.getCompanyById(job.organizationId)
+    if (companyResponse.status === 500) {
+        router.push({
+            name: 'jobs'
+        })
+        return
+    }
     const company = companyResponse.data
     state.job = job
     state.company = company
